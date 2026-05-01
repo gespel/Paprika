31095cc01ccb8cc88ca94e3071e48863a6e32d6a
gespel <sten.heimbrodt@hotmail.de>
2026-05-01 08:07:20 +0200

    adding request without context method

--- src/model/request.rs
+++ src/model/request.rs
@@ -57,7 +57,48 @@ impl ModelRequester {
 
         let r = client.post("http://127.0.0.1:11434/api/generate").body(payload_json).send().await?;
         return r.text().await;
-    } 
+    }
+
+    pub async fn request_full_text_without_context(&mut self, prompt: &str) -> String {
+        let client = Client::new();
+        
+        log::info!("Request to model: {}", prompt);
+
+        let payload = GenerateRequest {
+            model: self.model_name.clone(),
+            prompt: prompt.to_string()
+        };
+        let payload_json = serde_json::to_string(&payload).unwrap();
+
+        let r = client.post("http://127.0.0.1:11434/api/generate").body(payload_json).send().await;
+        let response = r.expect("").text().await;
+
+        let mut out: String = String::new();
+        match response {
+            Ok(r) => {
+                let lines: Vec<&str> = r.split("\n").collect();
+                for line in lines {    
+                    match serde_json::from_str::<serde_json::Value>(line) {
+                        Ok(j) => {
+                            if let Some(response) = j.get("response") {
+                                out.push_str(response.as_str().unwrap_or(""));
+                            }
+                        }
+                        Err(e) => {
+                            if !line.is_empty() {
+                                log::warn!("{}, {:?}", line, e);
+                            }
+                        }
+                    }
+                }
+                out
+            },
+            Err(e) => {
+                log::error!("{:?}", e);
+                e.to_string()
+            }
+        }
+    }
 
     pub async fn request_full_text(&mut self, prompt: &str) -> String {
         let mut out: String = String::new();
